871333c7c5ba4895bf4b6cae56f2436dfd0f5b45
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-04-10 10:38:19 -0300

    first commit

---
+++ app/home/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios"; // Importação do axios
+
+import CharacterCard from "../../components/CharacterCard";
+
+export default function Home() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character')
+            .then(response => {
+                setCharacters(response.data.results);
+            })
+            .catch(error => {
+                console.error("Erro ao buscar personagens: ", error);
+            });
+    }, []); // Adicionado array de dependências vazio para evitar chamadas infinitas, ativando apenas uma vez só.
+
+    console.log(characters) // Para verificar se os dados estão sendo recebidos corretamente.
+
+    return (
+        <div>
+            {characters.map((char) => (
+                <CharacterCard key={char.id} character={char} />
+            ))}
+        </div>
+    );
+}
